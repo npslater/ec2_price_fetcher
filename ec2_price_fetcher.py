6703c12c6156ce3b0cb34533bedbfe333105c8ad
EC2 Default User <ec2-user@ip-172-31-36-139.us-west-2.compute.internal>
2022-11-10 15:46:26 +0000

    added command line arg parsing

--- ec2_price_fetcher.py
+++ ec2_price_fetcher.py
@@ -1,5 +1,61 @@
 import boto3
 import json
 import io
-from multiprocessing import pool
+from multiprocessing.pool import ThreadPool as Pool
+import argparse
 
+SERVICE_CODE = 'AmazonEC2'
+PRODUCT_FAMILY = 'Compute Instance'
+
+def upload(s3, bucket_name, p,i,partition):
+    product = json.loads(p)
+    f = io.BytesIO(p.encode())
+    object_key = '{}/{}.offerfile.json'.format(partition, product['product']['sku'])
+    s3.upload_fileobj(f, bucket_name, object_key)
+    return '{} ({})'.format(object_key, i)
+    
+def success(sku): 
+    print(sku)
+    
+def fail(e):
+    print('Failed: {0}'.format((e)))
+    
+def main():
+    
+    parser = argparse.ArgumentParser(
+        prog = 'EC2 Price Fetcher',
+        description = 'Fetches offer files for EC2 instance pricing from the AWS Pricing API and writes them to S3')
+        
+    parser.add_argument('bucket_name')
+    parser.add_argument('-r', '--s3_region', default='us-east-1')
+    args = parser.parse_args()
+    
+    if not args.bucket_name:
+        return parser.usage()
+    
+    client = boto3.client('pricing', region_name='us-east-1')
+    s3 = boto3.client('s3', region_name=args.s3_region)
+    
+    filters = [
+        {
+            'Type': 'TERM_MATCH',
+            'Field': 'productFamily',
+            'Value': PRODUCT_FAMILY
+        }
+    ]
+    
+    paginator = client.get_paginator('get_products')
+    page_iterator = paginator.paginate(
+        ServiceCode = SERVICE_CODE,
+        Filters = filters)
+        
+    i = 0
+    with Pool(processes=20) as pool:
+        for page in page_iterator:
+            for p in page['PriceList']:
+                i += 1
+                partition = (i%25) + 1
+                pool.apply_async(upload, (s3,args.bucket_name,p,i,partition), {}, success, fail)
+                
+if __name__ == '__main__':
+    main()
